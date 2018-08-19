f85fea486e455da75a334c8700e5389e0ab7c226
Junichi Ito <jit@sonicgarden.jp>
2018-08-19 18:49:41 +0900

    Refactoring spec

--- spec/calc_tax_sandbox/item_spec.rb
+++ spec/calc_tax_sandbox/item_spec.rb
@@ -3,37 +3,39 @@ require 'date'
 RSpec.describe CalcTaxSandbox::Item do
   describe '#price_with_tax' do
     subject { item.price_with_tax(on: date) }
+    let(:date_20190930) { Date.new(2019, 9, 30) }
+    let(:date_20191001) { Date.new(2019, 10, 1) }
     context '食品でも新聞でもない場合' do
       let(:item) { CalcTaxSandbox::Item.new('プロを目指す人のためのRuby入門', 2980) }
-      context '2019-09-30の場合' do
-        let(:date) { Date.new(2019, 9, 30)  }
+      context '税率変更前の場合' do
+        let(:date) { date_20190930 }
         it { is_expected.to eq 3218 }
       end
-      context '2019-10-01の場合' do
-        let(:date) { Date.new(2019, 10, 1)  }
+      context '税率変更後の場合' do
+        let(:date) { date_20191001 }
         it { is_expected.to eq 3278 }
       end
     end
     context '食品だった場合' do
       context '酒類だった場合' do
         let(:item) { CalcTaxSandbox::Food.new('ビール', 300, alcohol: true) }
-        context '2019-09-30の場合' do
-          let(:date) { Date.new(2019, 9, 30)  }
+        context '税率変更前の場合' do
+          let(:date) { date_20190930 }
           it { is_expected.to eq 324 }
         end
-        context '2019-10-01の場合' do
-          let(:date) { Date.new(2019, 10, 1)  }
+        context '税率変更後の場合' do
+          let(:date) { date_20191001 }
           it { is_expected.to eq 330 }
         end
       end
       context '酒類でなかった場合' do
         let(:item) { CalcTaxSandbox::Food.new('ハンバーガー', 300) }
-        context '2019-09-30の場合' do
-          let(:date) { Date.new(2019, 9, 30)  }
+        context '税率変更前の場合' do
+          let(:date) { date_20190930 }
           it { is_expected.to eq 324 }
         end
-        context '2019-10-01の場合' do
-          let(:date) { Date.new(2019, 10, 1)  }
+        context '税率変更後の場合' do
+          let(:date) { date_20191001 }
           it { is_expected.to eq 324 }
           context '外食だった場合' do
             subject { item.price_with_tax(on: date, eating_out: true) }
@@ -45,23 +47,23 @@ RSpec.describe CalcTaxSandbox::Item do
     context '新聞の定期購読だった場合' do
       context '週1回発行の場合' do
         let(:item) { CalcTaxSandbox::NewspaperSubscription.new('Ruby新聞', 5000, per_week: 1) }
-        context '2019-09-30の場合' do
-          let(:date) { Date.new(2019, 9, 30)  }
+        context '税率変更前の場合' do
+          let(:date) { date_20190930 }
           it { is_expected.to eq 5400 }
         end
-        context '2019-10-01の場合' do
-          let(:date) { Date.new(2019, 10, 1)  }
+        context '税率変更後の場合' do
+          let(:date) { date_20191001 }
           it { is_expected.to eq 5500 }
         end
       end
       context '週2回発行の場合' do
         let(:item) { CalcTaxSandbox::NewspaperSubscription.new('Ruby新聞', 5000, per_week: 2) }
-        context '2019-09-30の場合' do
-          let(:date) { Date.new(2019, 9, 30)  }
+        context '税率変更前の場合' do
+          let(:date) { date_20190930 }
           it { is_expected.to eq 5400 }
         end
-        context '2019-10-01の場合' do
-          let(:date) { Date.new(2019, 10, 1)  }
+        context '税率変更後の場合' do
+          let(:date) { date_20191001 }
           it { is_expected.to eq 5400 }
         end
       end
